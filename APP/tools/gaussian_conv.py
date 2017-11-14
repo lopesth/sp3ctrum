a59c32df761db3c5e056c32f7e2a7bf20f6cc790
Thiago Lopes <thiagolopes@Thiagos-MacBook-Pro.local>
2017-11-14 10:46:34 -0200

    Small Corrections

--- APP/tools/gaussian_conv.py
+++ APP/tools/gaussian_conv.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-__author__ = "Thiago Lopes and LEEDMOL group"
-__credits__ = ["LOPES, T. O.", "OLIVEIRA, H. C. B."]
+__author__ = ["Thiago Lopes", "Daniel Machado", "Heibbe Oliveira"]
+__credits__ = "LEEDMOL group - Institute of Chemistry at Universidade de Brasília"
 __maintainer__ = "Thiago Lopes"
 __email__ = "lopes.th.o@gmail.com"
-__date__ = "Nov 11 of 2017"
-__version__ = "1.0"
+__date__ = "Nov 14 of 2017"
+__version__ = "1.0.1"
 
 import numpy, math
 
