ff43c3db63569871a802dc3273b0e5df619a08f8
Thiago O. Lopes <lopes.th.o@gmail.com>
2017-11-17 18:24:20 -0200

    Closing version 2.0.1

--- APP/tools/gaussian_conv.py
+++ APP/tools/gaussian_conv.py
@@ -3,7 +3,7 @@ __author__ = ["Thiago Lopes", "Daniel Machado", "Heibbe Oliveira"]
 __credits__ = "LEEDMOL group - Institute of Chemistry at Universidade de Brasilia"
 __maintainer__ = "Thiago Lopes"
 __email__ = "lopes.th.o@gmail.com"
-__date__ = "Nov 14 of 2017"
+__date__ = "Nov 17 of 2017"
 __version__ = "2.0.1"
 
 import numpy, math
