95165410e9297d11fde2dac67e3dbd70f333f307
Thiago O. Lopes <lopes.th.o@gmail.com>
2017-11-22 09:06:39 -0200

    finishing the orbitals.py
    class Transitions: all methods implemented

--- APP/tools/orbitals.py
+++ APP/tools/orbitals.py
@@ -1,4 +1,4 @@
-from find_a_string_in_file import Find_a_String
+from APP.tools.find_a_string_in_file import Find_a_String
 import sys
 
 class Transitions(object):
@@ -42,7 +42,7 @@ class Transitions(object):
         return translatedOrbital
 
     def getTransitions(self):
-        return [self.excitedState, self.wavelength, self.transleted_transitions, self.contrib_percent]
+        return [self.excitedState, self.wavelength, self.osc_force ,self.transleted_transitions, self.contrib_percent]
 
 class FrontierOrbitals(object):
 
@@ -68,6 +68,7 @@ class TransitionContribution(object):
         self.states = []
         self.font_orbitals = []
         self.makeTransitions()
+        self.getTransitions()
 
     def makeTransitions(self):
         myFile = open(self.file, 'r').readlines()
@@ -86,9 +87,45 @@ class TransitionContribution(object):
             self.states.append(Transitions(state+1, y[6], y[8].split("=")[1], transitions_in_file, cont_transtions, self.orbitals.homo))
 
     def getTransitions(self):
-        x = []
+        self.transitions_ord_by_state = []
         for state in self.states:
-            x.append(state.getTransitions())
+            self.transitions_ord_by_state.append(state.getTransitions())
+        return self.transitions_ord_by_state
+
+    def getTransitionsOsc(self):
+        x = []
+        self.transitions_ord_by_osc = []
+        for state in self.transitions_ord_by_state:
+           x.append(state[2])
+        x.sort(reverse=True)
+        i = 0
+        while i < (len(x)-1):
+            ii = 0
+            for state in self.transitions_ord_by_state:
+                if x[i] == state[2]:
+                    self.transitions_ord_by_osc.append(state)
+                    ii=1
+            if ii == 1:
+                i = i +1
+        return self.transitions_ord_by_osc
+
+    def getTransitionsWL(self):
+        x = []
+        self.transitions_ord_by_wl = []
+        for state in self.transitions_ord_by_state:
+           x.append(state[1])
+        x.sort()
+        i = 0
+        while i < (len(x)-1):
+            ii = 0
+            for state in self.transitions_ord_by_state:
+                if x[i] == state[1]:
+                    self.transitions_ord_by_wl.append(state)
+                    ii=1
+            if ii == 1:
+                i = i +1
+        return self.transitions_ord_by_wl
+
 
 if __name__ == "__main__":
     #file_ = "/Users/thiagolopes/Downloads/TD_Epinefrina_Tuned_LC-wPBE_49000.log"
@@ -96,5 +133,6 @@ if __name__ == "__main__":
     #w = FrontierOrbitals(file_)
 
     file__ = "/Users/thiagolopes/Downloads/TD_Epinefrina_LC-wPBE_49000_OPT.log"
-    x = TransitionContribution(file__).getTransitions()
-    w = FrontierOrbitals(file__)
+    x = TransitionContribution(file__)
+    w = x.getTransitionsWL()
+    print(w)
