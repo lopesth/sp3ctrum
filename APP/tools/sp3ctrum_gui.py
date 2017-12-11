84e7bc2a89d104ce7abb9db19bb209e662741661
Thiago O. Lopes <lopes.th.o@gmail.com>
2017-12-11 16:53:22 -0200

    makeSpectrumMD
    concertei o crash do espectro em MD

--- APP/tools/sp3ctrum_gui.py
+++ APP/tools/sp3ctrum_gui.py
@@ -506,7 +506,7 @@ class Application(Frame):
 
     def makeSpectrumMD(self):
         self.pyplot_bt.configure(state=NORMAL)
-        self.output_file_names=self.output_file_name
+        self.output_file_names=[self.output_entry.get()]
         error = self.getSimpleValues()
         if error < 1:
             self.spectrumUnited()
@@ -568,8 +568,8 @@ class Application(Frame):
                 self.wl_rang[1], self.title_chart, int( self.entry_res.get()),
                 self.osc_color, self.curve_color, "0", self.filenames,  self.plottypes.get())
             x.print_matplotlib()
-            if self.evol_plot_wl_choice.get == 1 or  self.evol_plot_osc_choice == 1:
-                y = PlotTransitions(self.target_dir, self.output_file_names, self.title_chart_evolution, self.filenames, self.evol_plot_wl_choice.get(), self.evol_plot_osc_choice,get())
+            if self.evol_plot_wl_choice.get() == 1 or  self.evol_plot_osc_choice.get() == 1:
+                y = PlotTransitions(self.target_dir, self.output_file_names, self.title_chart_evolution, self.filenames, self.evol_plot_wl_choice.get(), self.evol_plot_osc_choice.get())
 
 
 
