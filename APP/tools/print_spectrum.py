395da5de29e2756ec1a4b4e0d59ae8889a336f05
brittosandro <brittosandro@gmail.com>
2018-09-24 17:46:40 -0300

    name

--- APP/tools/print_spectrum.py
+++ APP/tools/print_spectrum.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-__author__ = ["Sandro Britto", "Mateus Barbosa", "Ueslei Vasconcelos", "Daniel Machado", "Thiago Lopes", "Heibbe Oliveira"]
+__author__ = ["Sandro Brito", "Mateus Barbosa", "Ueslei Vasconcelos", "Daniel Machado", "Thiago Lopes", "Heibbe Oliveira"]
 __credits__ = "LEEDMOL group - Institute of Chemistry at Universidade de Brasilia and Universidade Federal de Goiás"
-__maintainer__ = ["Thiago Lopes", "Sandro Britoo"]
+__maintainer__ = ["Thiago Lopes", "Sandro Brito"]
 __email__ = ["lopes.th.o@gmail.com", "brittosandro@gmail.com"]
 __date__ = "Set 28 of 2018"
 __version__ = "1.0.0"
