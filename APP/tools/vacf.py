8d0e28acb05c1acfccf6e1b39fe5aef8b72bbfab
Thiago O. Lopes <lopes.th.o@gmail.com>
2018-10-02 17:30:47 -0300

    VACF

--- APP/tools/vacf.py
+++ APP/tools/vacf.py
@@ -6,21 +6,64 @@ __email__ = ["lopes.th.o@gmail.com", "brittosandro@gmail.com"]
 __date__ = "Oct 02 of 2018"
 __version__ = "1.0.0"
 
+from matplotlib import pyplot
+
 class Velocity_AutoCor_Function(object):
 
-    def __init__(self, fileName, goalTime, fileTipe = "CPMD"):
+    def __init__(self, fileName, goalFrame, fileTipe = "CPMD"):
         self.fileName = fileName
-        self.goalTime = goalTime
+        self.goalFrame = goalFrame
         self.fileTipe = fileTipe
-        self.N = 1 #número de átomos -> vai ser mudado
+        self.N = 0
+        self.takeValues()
+        self.velocities = []
+        for frame in self.frames:
+            self.velocities.append(self.vacf(frame))
         
-    def vacf(self, time):
-        for atom in self.frames[time]:
-            cvv = self.vel(atom, 0) * self.getVel(atom, time)
-        cvv = (1/self.N) * cvv
+    def takeValues(self):
+        atomN = 0
+        frameList = []
+        self.frames = []
+        totalLength = 0
+        with open(self.fileName, "r", encoding="utf8", errors = "ignore") as myFile: 
+            for num, line in enumerate(myFile):
+                lineS = line.split()
+                if int(lineS[0]) != atomN:
+                    if len(frameList) != 0:
+                        self.frames.append(frameList)
+                        if self.N == 0:
+                            self.N = len(self.frames[0])
+                            totalLength = self.N * self.goalFrame
+                    atomN = int(lineS[0])
+                    frameList = []
+                    frameList.append(lineS[4:])
+                else:
+                    frameList.append(lineS[4:])
+                    if num+1 == totalLength:
+                        self.frames.append(frameList)
+                        break
+        if self.goalFrame > len(self.frames):
+            print("The TRAJECT file has " + str(len(self.frames)+1) + " frames, so there is no way to use " + str(self.goalFrame) + " frames.")
+            exit()
 
-    def getVel(self, atom, time):
-        
+    def vacf(self, frame):
+        cvv = 0
+        for atom in range(0, 4):
+            cvv = cvv + self.escalarProductVector(self.frames[0][atom], frame[atom]) / self.vectorMagnitude(self.frames[0][atom])
+        return (1/self.N) * cvv
+
+    def escalarProductVector(self, vector1, vector2):
+        return float(vector1[0]) * float(vector2[0]) + float(vector1[1]) * float(vector2[1]) + float(vector1[2]) * float(vector2[2])
+
+    def vectorMagnitude(self, vector):
+        return (float(vector[0])**2 + float(vector[1])**2 + float(vector[2])**2) ** (0.5)
 
 
-
+if __name__ == "__main__":
+    y = Velocity_AutoCor_Function("/Users/thiagolopes/Downloads/OneDrive-2018-10-02-2/TRAJECTORY", 20000).velocities
+    y_max = max(y)
+    yNorm = [element/y_max for element in y]
+    x = [x_i for x_i in range(1, 20000+1)]
+    plot = pyplot.plot(x, yNorm)
+    pyplot.show()
+    
