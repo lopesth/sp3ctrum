13fe7929bc0457d1c49ac03b3d6162fbbf05c148
brittosandro <brittosandro@gmail.com>
2018-10-03 18:19:48 -0300

    Return Modo -Friendly end -file in file sp3ctrum_app.py

--- sp3ctrum_app.py
+++ sp3ctrum_app.py
@@ -14,11 +14,18 @@ from tkinter import *
 def control_the_flux(choice_interface, file_name):
     program = Sp3ctrum_UVvis_P4tronum(__version__)
 
-    if choice_interface == "-gui" or choice_interface == "":
+    if choice_interface == "-file":
+        program.run_fed_terminal(file_name)
+
+    elif choice_interface == "-friendly":
+        program.run_friendly_terminal()
+
+    elif choice_interface == "-gui" or choice_interface == "":
         root = Tk()
         root.title("UV-Vis Sp3ctrum P4tronum "+__version__)
         app = Application(root)
         mainloop()
+        
     else:
         print("Unrecognized Keyword")
         print("Type -file, -friendly or -gui.")
@@ -31,7 +38,9 @@ if __name__ == "__main__":
         file_name = sys.argv[2]
     except:
         try:
-            if choice_interface == "-gui" or choice_interface == "":
+            if choice_interface == "-friendly":
+                file_name = ""
+            elif choice_interface == "-gui" or choice_interface == "":
                 file_name = ""
         except:
             choice_interface = ""
